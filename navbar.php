8a7216693b03302872281b887b8e743e869fb0c4
Josfel2015 <153402917+Josfel2015@users.noreply.github.com>
2024-04-12 11:15:10 +0100

    navbar

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<nav class="nav-bar">
+                <img src="./img/Chrislaw.PNG" class="brand-name">
+                <a href="#" class="hamburger">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+                <div class="menu-bar">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <div class="dropdown">
+                            <li class="dropbtn"><a href="index.php">Category <i class="fa fa-caret-down"></i></a></li>
+                            <div class="dropdown-content">
+                                <a href="ingredient.php">Ingredient</a>
+                                <a href="class.php">Class</a>
+                                <a href="season.php">Season</a>
+                                <a href="diet.php">Diet</a>
+                            </div>
+                        </div>
+                        <div class="dropdown">
+                            <li class="dropbtn"><a href="index.php">Location <i class="fa fa-caret-down"></i></a></li>
+                            <div class="dropdown-content">
+                                <a href="abuja.php">Abuja</a>
+                                <a href="lagos.php">Lagos</a>
+                            </div>
+                        </div>
+                        <li><a href="about.php">About Us</a></li>
+                        <li><a href="contactus.php">Contact</a></li>
+                        <li><a href="login.php">Login/Register</a></li>
+                    </ul>
+                </div>
+            </nav>
