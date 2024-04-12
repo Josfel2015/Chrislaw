1c92c55d0a066e34b09c41672de26a8ff44faad9
Josfel2015 <153402917+Josfel2015@users.noreply.github.com>
2024-04-12 13:00:14 +0100

    add recipies list

--- admin/add_recipe_list.php
+++ admin/add_recipe_list.php
@@ -1,4 +1,32 @@
-﻿<!DOCTYPE html>
+﻿<?php 
+    include('check_user.php');
+    require_once('../db/db_connection.php');
+    if (isset($_POST['send'])) {
+        $recipe_cat = $_POST['recipe_cat'];
+        $recipe_name = $_POST['recipe_name'];
+        // $recipe_img = $_POST['recipe_img'];
+        $ingredient =$_POST['ingredient'];
+        $instruction = $_POST['instruction'];
+        $portion = $_POST['portion'];
+
+
+        $recipe_img = $_FILES["recipe_img"]["name"];
+        $folder = "../img/".$recipe_img;
+        $temp = $_FILES['recipe_img']['tmp_name'];
+        move_uploaded_file($temp, $folder);
+
+        $sql = "INSERT INTO reci_list(recipe_cat,recipe_name,recipe_img,ingredient,instruction,portion)VALUES(?,?,?,?,?,?)";
+        $qry = $conn->prepare($sql);
+        $qry->bind_param('ssssss',$recipe_cat,$recipe_name,$recipe_img,$ingredient,$instruction,$portion);
+        $qry->execute();
+        if ($qry) {
+            echo "<script>alert('Recipe successfylly added')</script>";
+        }else {
+            echo "<script>alert('Please check your connection')</script>";
+        }
+    }
+?>
+<!DOCTYPE html>
 <html>
 <head>
       <meta charset="utf-8" />
@@ -30,26 +58,42 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <form role="form">
+                                    <form role="form" action="add_recipe_list.php" enctype="multipart/form-data" method="POST">
                                         <div class="form-group">
-                                            <label>Recipe</label>
-                                            <select class="form-control">
-                                                <option>One Vale</option>
-                                                <option>Two Vale</option>
-                                                <option>Three Vale</option>
-                                                <option>Four Vale</option>
+                                            <label>Recipe Category</label>
+                                           <select name="recipe_cat" class="form-control">
+                                            <option value="">Select</option>
+                                           <?php 
+                                                $sql = "SELECT * FROM reci_cat";
+                                                $qry = $conn->query($sql); 
+                                                while ($row = $qry->fetch_assoc()) {
+                                            ?>
+                                            <option value="<?php echo $row['recipe_cat']; ?>"><?php echo $row['recipe_cat']; ?></option>
+                                             <?php }?>
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <label>Input Recipe picture</label>
-                                            <input type="file" />
+                                            <label for="">Recipe Name</label>
+                                            <input type="text" name="recipe_name" class="form-control" placeholder="enter recipe name" required>
                                         </div>
                                         <div class="form-group">
-                                            <label>Recipe Description</label>
-                                            <textarea class="form-control" rows="3"></textarea>
+                                            <label>Input Recipe picture</label>
+                                            <input type="file" name="recipe_img" required/>
                                         </div>
-                                        <button type="submit" class="btn btn-default">Submit Button</button>
-                                        <button type="reset" class="btn btn-primary">Reset Button</button>
+                                        <div class="form-group">
+                                            <label>Recipe ingredients</label>
+                                            <textarea name="ingredient" class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Recipe Instruction</label>
+                                            <textarea name="instruction" class="form-control" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Portion</label>
+                                            <input type="text" name="portion" class="form-control" placeholder="enter portion" required>
+                                        </div>
+                                        <input type="submit" value="Submit" name="send" class="btn btn-success">
+                                        <button type="reset" class="btn btn-danger">Reset Button</button>
 
                                     </form>
                                 </div>
