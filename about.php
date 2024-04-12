632d00f70fc9de2c8a0d7378a41e6e8ff392bcb3
Josfel2015 <153402917+Josfel2015@users.noreply.github.com>
2024-04-12 11:26:59 +0100

    abt

--- about.php
+++ about.php
@@ -10,6 +10,7 @@
         <style>
            .myabout {
                 margin-top: 10%;
+                margin-bottom: 9%;
                 display: inline-flex;
                 padding: 10px;
                 clear: both;
@@ -31,8 +32,30 @@
             #about-right {
                 display: flex;
                 justify-content: center;
-                font-size: 2rem;
+                font-size: 1rem;
             }
+            .dropdown:hover .dropdown-content {
+                display: block;
+            }
+            .dropdown-content {
+                display: none;
+                position: absolute;
+                background-color: #f9f9f9;
+                min-width: 160px;
+                box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+                z-index: 1;
+                }
+                ul a:hover, .dropdown:hover .dropbtn {
+                background-color: red;
+                }
+                .dropdown-content a {
+                float: none;
+                color: black;
+                padding: 12px 16px;
+                text-decoration: none;
+                display: block;
+                text-align: left;
+                }
         </style>
         <title>Chrislaw</title>
     </head>
@@ -40,22 +63,7 @@
 
         <!--- Header -->
         <div class="header-login">
-            <nav class="nav-bar">
-                <img src="./img/Chrislaw.PNG" class="brand-name">
-                <a href="#" class="hamburger">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                </a>
-                <div class="menu-bar">
-                    <ul>
-                        <li><a href="index.php">Home</a></li>
-                        <li><a href="about.php">About</a></li>
-                        <li><a href="contactus.php">Contact</a></li>
-                        <li><a href="login.php">Login/Register</a></li>
-                    </ul>
-                </div>
-            </nav>
+            <?php include('navbar.php')?>
             <!-- Search Bar -->
         </div>
         
@@ -63,15 +71,26 @@
         <section class="myabout">
             <div id="about-left"></div>
             <div id="about-right">
-            <p style="display: contents;font-weight:700;color: #1c2541;">About US</p> <br/> 
-            NAME: JOSEPH OLOJEDE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            MATRIC NO: 10927363<br/>  
-            <p style="display: contents; font-weight:700; color:blueviolet">OUR STORY</p> <BR/>     
-            We're not just another website; we are your culinary companion on a journey of taste and creativity! At Chrislaw, we are committed to simplifying the art of cooking for all food enthusiasts.<br>
-            Whether you are cooking solo, with friends, or hosting a cookathon, we offer a host of meticulously researched, tested, and beloved recipes curated from a diverse community of food lovers. But that's not all – just like the perfect icing on a cake, we provide invaluable cooking instructions and meal serving techniques to elevate your culinary experience. Whether you are a seasoned chef or a curious recipe seeker, we are here to delight your palate and inspire your kitchen adventures. 
-            <br/>    
-            <p style="display: contents; font-weight:700; color: #1c2541;">cooking with Chrislaw! #Cookathon!</p>      
-        </div>
+                <p style="display: contents;font-weight:500;color: #1c2541;">About US</p> <br/> 
+                 We're not just another website; we are your culinary companion on a journey of 
+                taste and creativity! At Chrislaw, we are committed to simplifying the art of
+                cooking for all food enthusiasts.<br>
+                Whether you are cooking solo, with friends, or hosting a cookathon, we offer a 
+                host of meticulously researched, tested, and beloved recipes curated from a 
+                diverse community of food lovers. But that's not all – just like the perfect 
+                icing on a cake, we provide invaluable cooking instructions and meal serving 
+                techniques to elevate your culinary experience. Whether you are a seasoned chef 
+                or a curious recipe seeker, we are here to delight your palate and inspire your 
+                kitchen adventures. 
+                <br/>    
+                <p style="display: contents; font-weight:500; color: #1c2541;">
+                    cooking with Chrislaw! #Cookathon!
+                </p>  
+                <br>    
+                <p style="display: contents; font-weight:500; color:blueviolet">
+                    NAME: JOSEPH OLOJEDE 
+                </p> <BR/>     
+            </div>
         </section>
         <!--Ending of Recipe Details-->
 
@@ -85,7 +104,7 @@
         </div>
         <div class="quick-bar">
             <a href="index.php">Home</a>
-            <a href="about.php">About</a>
+            <a href="about.php">About Us</a>
             <a href="contactus.php">Contact</a>
             <a href="login.php">Login/Register</a>
         </div>
